365003b75be5203d19a8b2c3f143e8a0f2c13183
Y. S. Warkare <52399544+yswarkare@users.noreply.github.com>
2020-09-27 11:49:48 +0530

    MERN Stack Online Shopping Site moved to new Repository

--- MERN_Stack/Online_Shopping_Site/client/src/redux/actions/error_types.js
+++
@@ -1,8 +0,0 @@
-
-// Error Types
-
-export const Get_Error = "Get_Error";
-export const Registration_Errors = 'Registration_Errors';
-export const Log_In_Error = 'Log_In_Error';
-export const Is_User_Logged_In_Error = 'Is_User_Logged_In_Error';
-export const Log_Out_Error = 'Log_Out_Error';
